a2f4212a3070dd9d53500da4b7828a71f67ca630
PuzanovArtem <puzanov522@gmail.com>
2024-08-22 20:18:36 +0300

    main tour

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import { headerSlider } from './src/js/libs/swiper';
 import '/src/js/components/header.js';
 import '/src/js/modules/counter.js';
-
-import '/src/js/components/search.js';
+import './src/js/components/search.js';
 
 
 document.addEventListener('DOMContentLoaded',()=>{
     headerSlider()
+});
+
+import {mainCompanySlider  } from './src/js/libs/swiper';
+document.addEventListener('DOMContentLoaded',()=>{
+    mainCompanySlider()
+})
+
+import { mainHotSlider  } from './src/js/libs/swiper';
+document.addEventListener('DOMContentLoaded',()=>{
+    mainHotSlider()
 })
--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,5 +1,7 @@
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
+import { Autoplay } from "swiper/modules";
+
 
 export const headerSlider = () => {
   try {
@@ -18,6 +20,82 @@ export const headerSlider = () => {
     };
 
     const headerSwiper = new Swiper(".header__slider", config);
+  } catch (error) {
+    console.log("Swiper initialization error:", error);
+  }
+};
+
+export const mainCompanySlider = () => {
+  try {
+    const config = {
+      loop: true,
+      navigation: {
+        nextEl: ".main-company__swiper-button-next",
+        prevEl: ".main-company__swiper-button-prev",
+        enabled: true,
+      },
+      touchEventsTarget: 'wrapper',
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        
+        804: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        991: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 11,
+        },
+      },
+    };
+
+    const mainCompanySwiper = new Swiper(".main-company__swiper", config);
+  } catch (error) {
+    console.log("Swiper initialization error:", error);
+  }
+};
+
+export const mainHotSlider = () => {
+  try {
+    const config = {
+      loop: true,
+      navigation: {
+        nextEl: ".main-hot__swiper-button-next",
+        prevEl: ".main-hot__swiper-button-prev",
+        enabled: true,
+      },
+      touchEventsTarget: 'wrapper',
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        
+        804: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        991: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 11,
+        },
+      },
+    };
+
+    const mainCompanySwiper = new Swiper(".main-company__swiper", config);
   } catch (error) {
     console.log("Swiper initialization error:", error);
   }
