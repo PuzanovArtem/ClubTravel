923cdecb5acb870e647479a2d79312ebf286db5c
PuzanovArtem <puzanov522@gmail.com>
2024-09-13 16:28:38 +0300

    code review

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ import {
   mainSummerSlider,
 } from './src/js/libs/swiper'
 
-import '/src/js/components/header.js'
+import {
+  header,
+} from './src/js/components/header.js'
+  
 import '/src/js/modules/counter.js'
 import './src/js/components/search.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+  header()
   headerSlider()
   mainCompanySlider()
   mainHotSlider()
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,15 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger");
-  const sidemenu = document.querySelector(".sidebar");
+export const header = () => {
+  const burger = document.querySelector('.burger');
+  const sideMenu = document.querySelector('.sidebar');
 
-  if (burger && sidemenu) {
-    burger.addEventListener("click", () => {
-      const isActive = sidemenu.classList.contains("sidebar_active");
-      document.body.style.overflow = isActive ? "" : "hidden";
-      sidemenu.classList.toggle("sidebar_active");
-      burger.classList.toggle("burger_active");
+  if (burger && sideMenu) {
+    burger.addEventListener('click', event => {
+      const isActive = sideMenu.classList.contains('sidebar_active');
+
+      document.body.style.overflow = isActive ? '' : 'hidden';
+      sideMenu.classList.toggle('sidebar_active');
+      burger.classList.toggle('burger_active');
     });
-  } else {
-    console.log("Burger or sidebar not found");
-  }
-});
+
+    document.addEventListener('click', event => {
+      if (
+        sideMenu.classList.contains('sidebar_active') &&
+        !sideMenu.contains(event.target) &&
+        !burger.contains(event.target)
+      ) {
+        sideMenu.classList.remove('sidebar_active');
+        burger.classList.remove('burger_active');
+        document.body.style.overflow = '';
+      }
+    });
+
+    return
+  } 
+
+    console.log('Burger or sidebar not found');
+  
+};
