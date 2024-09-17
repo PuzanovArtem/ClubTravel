5c15fb8336e68e8ca28ddd51eac89a1fae962ba3
PuzanovArtem <puzanov522@gmail.com>
2024-09-17 16:09:19 +0300

    add new Counter

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,12 +1,10 @@
 import { initializeDropdowns } from '/src/js/modules/search/dropdown'
-import { initializeCounters } from '/src/js/modules/search/counter'
 import { initializeDatePicker } from '/src/js/modules/search/datePicker'
 import { initializeAdvancedSearch } from '/src/js/modules/search/advancedSearch'
 import { priceSlider } from '/src/js/modules/search/priceSlider'
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeDropdowns()
-  initializeCounters()
   initializeDatePicker()
   initializeAdvancedSearch()
   priceSlider()
