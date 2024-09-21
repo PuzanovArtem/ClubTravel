a059f672d475bf197eb9e1ebc5e07514d45fef2e
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-09-21 09:59:46 +0300

    fix

--- src/pages/directions-tour/directions-tour.js
+++ src/pages/directions-tour/directions-tour.js
@@ -1,4 +1,5 @@
 import '../../js/components/search.js';
+import '../../js/modules/counter.js'
 import { header } from '../../js/components/header.js';
 
 document.addEventListener('DOMContentLoaded', () => {
--- src/pages/user-account/user-account.js
+++ src/pages/user-account/user-account.js
@@ -1,10 +1,4 @@
-import '../../js/components/search.js';
-import '../../js/modules/counter.js';
 import { header } from '../../js/components/header.js';
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     header();
