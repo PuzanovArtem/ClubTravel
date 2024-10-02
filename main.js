c50de44a0ee73a306b078de47004955f41bd37c8
PuzanovArtem <puzanov522@gmail.com>
2024-10-02 16:59:54 +0300

    test build

--- main.js
+++ main.js
@@ -24,34 +24,3 @@ document.addEventListener('DOMContentLoaded', () => {
     dataCounterType: 'child',
   })
 })
-
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger')
-  const sideMenu = document.querySelector('.sidebar')
-
-  if (!burger || !sideMenu) {
-    console.error('Burger or sidebar not found')
-    return
-  }
-
-  burger.addEventListener('click', () => {
-    const isActive = sideMenu.classList.contains('sidebar_active')
-
-    document.body.style.overflow = isActive ? '' : 'hidden'
-    sideMenu.classList.toggle('sidebar_active')
-    burger.classList.toggle('burger_active')
-  })
-
-  document.addEventListener('click', event => {
-    const isOutClick =
-      sideMenu.classList.contains('sidebar_active') &&
-      !sideMenu.contains(event.target) &&
-      !burger.contains(event.target)
-
-    if (isOutClick) {
-      sideMenu.classList.remove('sidebar_active')
-      burger.classList.remove('burger_active')
-      document.body.style.overflow = ''
-    }
-  })
-})
