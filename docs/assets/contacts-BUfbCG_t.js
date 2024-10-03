cb95797ce0fa4758e915e0b1c4ad6757fd90775b
PuzanovArtem <puzanov522@gmail.com>
2024-10-03 17:34:03 +0300

    test build

---
+++ docs/assets/contacts-BUfbCG_t.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{c as t}from"./validator-DLzj4qSZ.js";import{h as o}from"./header-CDcaaDmM.js";document.addEventListener("DOMContentLoaded",()=>{o(),t()});
--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,11 +1,11 @@
 import { initializeDropdowns } from '/src/js/modules/search/dropdown'
-import { initializeDatePicker } from '/src/js/modules/search/datePicker'
+// import { initializeDatePicker } from '/src/js/modules/search/datePicker'
 import { initializeAdvancedSearch } from '/src/js/modules/search/advancedSearch'
-import { priceSlider } from '/src/js/modules/search/priceSlider'
+// import { priceSlider } from '/src/js/modules/search/priceSlider'
 
 export const initSearchBar = () => {
   initializeDropdowns()
-  initializeDatePicker()
+  // initializeDatePicker()
   initializeAdvancedSearch()
-  priceSlider()
+  // priceSlider()
 }
