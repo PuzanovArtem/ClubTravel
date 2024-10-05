343f6402d52a9112fe3897d244ce9a2dfa1dbfc1
PuzanovArtem <puzanov522@gmail.com>
2024-10-05 09:59:02 +0300

    add fix

--- src/pages/tour-request/tour-request.js
+++ src/pages/tour-request/tour-request.js
@@ -1,8 +1,8 @@
 import { header } from '/src/js/components/header.js'
-import { Counter, updateTotalChildren } from './src/js/modules/counter.js'
+// import { Counter, updateTotalChildren } from '/src/js/modules/counter.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   header()
 
-  new Counter({ selector: '#counter-3', initCount: 0, step: 1, displaySelector: '#adults-display' })
+  // new Counter({ selector: '#counter-3', initCount: 0, step: 1, displaySelector: '#adults-display' })
 })
