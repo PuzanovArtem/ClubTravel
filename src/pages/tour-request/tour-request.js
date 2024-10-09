119d41254bb1b5f710847a4c2c81f1ad90d8e470
PuzanovArtem <puzanov522@gmail.com>
2024-10-09 17:42:15 +0300

    added validation

--- src/pages/tour-request/tour-request.js
+++ src/pages/tour-request/tour-request.js
@@ -1,11 +1,13 @@
 import { Counter, updateTotalChildrenForm } from '../../js/modules/counter.js'
 import { Food } from '../../js/pages/tour-request/food.js'
 import { header } from '/src/js/components/header.js'
+import { initStarSelection } from '../../js/pages/tour-request/stars.js'
 import { initTabs } from '../../js/pages/tour-request/tabs.js'
 import { initializeDropCountry } from '/src/js/pages/tour-request/drop-country '
 import { initializeDropdowns } from '../../js/modules/search/dropdown.js'
 import { manageSelects } from '../../js/modules/manage-selects.js'
 import { select } from '../../js/components/select.js'
+import { tourValidator } from '../../js/libs/validator.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   header()
@@ -14,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
   initializeDropdowns()
   select
   initTabs()
+  initStarSelection()
+  tourValidator()
   new Food()
   new Counter({ selector: '#counter-10', initCount: 0, step: 1 })
 
