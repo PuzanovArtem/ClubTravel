7e61447575e98740398aa606189264e3f08fb7aa
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-10-09 20:34:21 +0300

    fix bug

--- docs/assets/authorization-C9-a464R.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{h as o}from"./header-CDcaaDmM.js";import{v as t}from"./validator-DLzj4qSZ.js";document.addEventListener("DOMContentLoaded",()=>{o(),t()});
