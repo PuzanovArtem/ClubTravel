07d47ebbb824dab808988062d5549548a2777468
PuzanovArtem <puzanov522@gmail.com>
2024-10-10 14:55:21 +0300

    deleted docs

--- docs/assets/forgotPassword-C3a_jPil.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{e}from"./validator-T4HusQx-.js";import{h as o}from"./header-CDcaaDmM.js";document.addEventListener("DOMContentLoaded",()=>{o(),e()});
--- docs/assets/registration-D2gkyUOT.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{h as o}from"./header-CDcaaDmM.js";import{v as t}from"./validator-T4HusQx-.js";document.addEventListener("DOMContentLoaded",()=>{o(),t()});
