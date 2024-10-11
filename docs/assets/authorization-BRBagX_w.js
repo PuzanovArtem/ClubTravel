60d555ee188d33db723f7e969e7c15710f370705
PuzanovArtem <puzanov522@gmail.com>
2024-10-11 19:08:39 +0300

    build fix

---
+++ docs/assets/authorization-BRBagX_w.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{h as o}from"./header-CDcaaDmM.js";import{v as t}from"./validator-kAhhofT2.js";import"./_commonjsHelpers-Cpj98o6Y.js";document.addEventListener("DOMContentLoaded",()=>{o(),t()});
