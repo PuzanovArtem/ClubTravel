9c42cdabca239df0a1d25d2dbfdd91d122fb5796
PuzanovArtem <puzanov522@gmail.com>
2024-10-14 19:16:58 +0300

    bug fix

---
+++ docs/assets/authorization-CHUEMBcq.js
@@ -0,0 +1,1 @@
+import{h as o}from"./header-DXygQjIQ.js";import{v as t}from"./validator-kAhhofT2.js";import"./_commonjsHelpers-Cpj98o6Y.js";document.addEventListener("DOMContentLoaded",()=>{o(),t()});
---
+++ docs/assets/textPage-ChF6Dleq.js
@@ -0,0 +1,1 @@
+import{h as e}from"./header-DXygQjIQ.js";document.addEventListener("DOMContentLoaded",()=>{e()});
