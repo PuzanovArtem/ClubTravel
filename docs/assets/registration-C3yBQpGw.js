72a5e44cacb8efcf998a879c8971b2242532bd08
PuzanovArtem <puzanov522@gmail.com>
2024-10-14 16:03:17 +0300

    deleted docs

--- docs/assets/registration-C3yBQpGw.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import{h as o}from"./header-CDcaaDmM.js";import{v as t}from"./validator-CTKmPxhu.js";document.addEventListener("DOMContentLoaded",()=>{o(),t()});
